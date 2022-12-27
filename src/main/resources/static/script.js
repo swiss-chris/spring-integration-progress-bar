9b683515c55113b27411fa16f31cac0656d72c97
Chris Dickinson <chris@dickinson.ch>
2022-12-27 11:40:35 +0100

    refactor & comment

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,14 +3,7 @@ const flowIds = [];
 let latestFlowId;
 
 const connect = () => {
-    sock = new SockJS('http://localhost:8080/messages')
-}
-
-const reconnect = () => {
-    if (sock.readyState === 3) {
-        connect();
-    }
-
+    sock = new SockJS('http://localhost:8080/messages');
     sock.onmessage = function ({data}) {
         const {flowId, percent} = JSON.parse(data);
         if (!flowIds.includes(flowId)) {
@@ -18,10 +11,19 @@ const reconnect = () => {
             latestFlowId = flowId;
         }
         if (flowId === latestFlowId) {
+            // only show the progress of the most recently started flow
             document.getElementById('progress-bar').style.width = percent + '%';
             document.getElementById('progress-bar').innerText = percent + '%';
         }
     };
+}
+
+const reconnect = () => {
+    const isSocketClosed = sock.readyState === 3;
+    if (isSocketClosed) {
+        // reconnects if the server was restarted
+        connect();
+    }
 }
 
 const startflow = () => {

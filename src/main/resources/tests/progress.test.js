7a76a255c4a6f523a8967e80fc399000191092c6
Chris Dickinson <chris@dickinson.ch>
2023-01-05 20:38:51 +0100

    fix unit tests

--- src/main/resources/tests/progress.test.js
+++ src/main/resources/tests/progress.test.js
@@ -3,14 +3,17 @@ const {Progress, Percent, Time} = require('../static/script/lib');
 describe('Progress', () => {
     const start = new Time(1672866000000); // 22:00:00
     const now = new Time(1672867800000); // 22:30:00
+    const later = new Time(1672956000000); // 23:00:00
     const percent = new Percent(50); // 30 minutes
     const progress = new Progress(start, now, percent);
 
     test('Progress.copy()', () => {
-        expect(progress.copy(percent).start().toString()).toBe(start.toString());
-        expect(progress.copy(percent).end().toString()).toBe('23:00:00');
-        expect(progress.copy(new Percent(25)).start().toString()).toBe('22:00:00');
-        expect(progress.copy(new Percent(25)).end().toString()).toBe('00:00:00');
+        expect(progress.copy(percent, now).start().toString()).toBe('22:00:00');
+        expect(progress.copy(percent, now).end().toString()).toBe("23:00:00");
+        expect(progress.copy(percent, later).start().toString()).toBe('22:00:00');
+        expect(progress.copy(percent, later).end().toString()).toBe("00:00:00");
+        expect(progress.copy(new Percent(25), now).start().toString()).toBe('22:00:00');
+        expect(progress.copy(new Percent(25), now).end().toString()).toBe('00:00:00');
     });
 
     test('Progress.isFinished()', () => {

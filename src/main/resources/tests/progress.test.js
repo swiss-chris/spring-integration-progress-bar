d0ef7fe0251cce3d05eaf85be203fe4d91d9d67c
Chris Dickinson <chris@dickinson.ch>
2023-01-05 21:16:17 +0100

    unit tests

--- src/main/resources/tests/progress.test.js
+++ src/main/resources/tests/progress.test.js
@@ -2,25 +2,26 @@ const {Progress, Percent, Time} = require('../static/script/lib');
 
 describe('Progress', () => {
     const start = new Time(1672866000000); // 22:00:00
-    const now = new Time(1672867800000); // 22:30:00
-    const later = new Time(1672956000000); // 23:00:00
+    const plus30 = new Time(1672867800000); // 22:30:00
+    const plus60 = new Time(1672956000000); // 23:00:00
+    const plus120 = new Time(1672959600000); // 00:00:00
     const percent = new Percent(50); // 30 minutes
-    const progress = new Progress(start, now, percent);
+    const progress = new Progress(start, plus30, percent);
 
     test('Progress.copy()', () => {
-        expect(progress.copy(percent, now).start().toString()).toBe('22:00:00');
-        expect(progress.copy(percent, now).end().toString()).toBe("23:00:00");
-        expect(progress.copy(percent, later).start().toString()).toBe('22:00:00');
-        expect(progress.copy(percent, later).end().toString()).toBe("00:00:00");
-        expect(progress.copy(new Percent(25), now).start().toString()).toBe('22:00:00');
-        expect(progress.copy(new Percent(25), now).end().toString()).toBe('00:00:00');
+        expect(progress.copy(percent, plus30).start().toString()).toBe(start.toString());
+        expect(progress.copy(percent, plus30).end().toString()).toBe(plus60.toString());
+        expect(progress.copy(percent, plus60).start().toString()).toBe(start.toString());
+        expect(progress.copy(percent, plus60).end().toString()).toBe(plus120.toString());
+        expect(progress.copy(new Percent(25), plus30).start().toString()).toBe(start.toString());
+        expect(progress.copy(new Percent(25), plus30).end().toString()).toBe(plus120.toString());
     });
 
     test('Progress.isFinished()', () => {
-        expect(new Progress(start, now, new Percent(0)).isFinished()).toBeFalsy();
-        expect(new Progress(start, now, new Percent(50)).isFinished()).toBeFalsy();
-        expect(new Progress(start, now, new Percent(99)).isFinished()).toBeFalsy();
-        expect(new Progress(start, now, new Percent(100)).isFinished()).toBeTruthy();
+        expect(new Progress(start, plus30, new Percent(0)).isFinished()).toBeFalsy();
+        expect(new Progress(start, plus30, new Percent(50)).isFinished()).toBeFalsy();
+        expect(new Progress(start, plus30, new Percent(99)).isFinished()).toBeFalsy();
+        expect(new Progress(start, plus30, new Percent(100)).isFinished()).toBeTruthy();
     });
 
     test('Progress.percent()', () => {

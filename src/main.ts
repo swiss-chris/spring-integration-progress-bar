ff5760058615e4856963e308fd9244e30e69965d
Chris Dickinson <chris@dickinson.ch>
2023-01-06 14:47:21 +0100

    initializing typescript

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const appContainer = document.getElementById("app");
+const el = document.createElement("div");
+el.innerHTML = `
+    <h1>Hello World</h1>
+   <span style="background-color: red; color: white;">
+     This is a web app built without frameworks
+   </span>
+ `;
+appContainer?.appendChild(el);

58422d20e8d60475e98a819d8447069fa64417e7
Chris Dickinson <chris@dickinson.ch>
2023-01-08 22:20:22 +0100

    custom dark theme style

--- src/main/typescript/page/page.ts
+++ src/main/typescript/page/page.ts
@@ -203,12 +203,12 @@ class Row {
     }
 
     private end(): void {
-        this.row.querySelector<HTMLElement>('.end')!.innerText = this.progress.end().date().toLocaleTimeString();
         if (this.progress.isFinished()) {
             this.row.querySelector<HTMLElement>('.end')!.classList.remove('dim');
         } else {
             this.row.querySelector<HTMLElement>('.end')!.classList.add('dim');
         }
+        this.row.querySelector<HTMLElement>('.end')!.innerText = this.progress.end().date().toLocaleTimeString();
     }
 }
 

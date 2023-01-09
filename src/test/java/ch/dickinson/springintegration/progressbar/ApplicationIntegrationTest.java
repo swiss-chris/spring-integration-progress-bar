386ec8dbdcbeeb1f4dd4f0e70265ed3c7ab2e9d0
Chris Dickinson <chris@dickinson.ch>
2023-01-09 22:22:56 +0100

    WIP: testing the application with Spring Boot and Selenium

--- src/test/java/ch/dickinson/springintegration/progressbar/ApplicationIntegrationTest.java
+++ src/test/java/ch/dickinson/springintegration/progressbar/ApplicationIntegrationTest.java
@@ -1,20 +1,25 @@
 package ch.dickinson.springintegration.progressbar;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ApplicationIntegrationTest {
 
+    public static final String TIME_PATTERN = "\\d{2}:\\d{2}:\\d{2}"; // e.g. 23:59:59
     private WebDriver driver;
 
     @Value(("${server.port}"))
@@ -29,9 +34,23 @@ class ApplicationIntegrationTest {
     @Test
     public void testTest() {
         driver.get("http://localhost:" + port);
-        Assertions.assertEquals("Spring Integration (Java DSL) Progress Bar, Using WebSockets", driver.getTitle());
+        assertEquals("Spring Integration (Java DSL) Progress Bar, Using WebSockets", driver.getTitle());
+
+        driver.findElement(By.xpath("//button[text() = 'Start Flow']")).click();
+
+        final String start = driver.findElement(By.className("start")).getText();
+        assertThat(start, matchesPattern(TIME_PATTERN));
+
+        final String sources = driver.findElement(By.className("sources")).getText();
+        assertThat(sources, is("all"));
+
+        final String categories = driver.findElement(By.className("categories")).getText();
+        assertThat(categories, is("all"));
+
+        final String duration = driver.findElement(By.className("duration")).getText();
+        assertThat(duration, matchesPattern(TIME_PATTERN));
+
         driver.quit();
     }
-
 }
 

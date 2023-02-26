89716bb02563bf32cd5d11ee7a948c6b48457b3b
Chris Dickinson <chris@dickinson.ch>
2023-02-26 11:06:32 +0100

    refactoring

--- src/main/typescript/util/host.ts
+++ src/main/typescript/util/host.ts
@@ -2,5 +2,5 @@ import { replaceAfterColon } from './string-utils';
 
 export const getBackendUrl = (): string => {
     const url = new URL(window.location.href);
-    return `${(url.protocol)}//${(replaceAfterColon(url.host, process.env.JAVA_PORT!))}`;
+    return `${(url.protocol)}//${(replaceAfterColon(url.host, process.env.BACKEND_PORT!))}`;
 };

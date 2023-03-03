c51cae330387c520291ab3448abccbbed454f5c5
Chris Dickinson <chris@dickinson.ch>
2023-03-03 18:55:46 +0100

    WIP: fix maven build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '../../resources/static'
+    outDir: '../resources/static'
   },
   define: {
     'process.env.BACKEND_PORT': process.env.BACKEND_PORT || 8080

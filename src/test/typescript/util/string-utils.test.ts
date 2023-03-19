4622ea9ff9f2b13cf07961f8c8167bc343fa939d
Chris Dickinson <chris@dickinson.ch>
2023-03-19 13:11:46 +0100

    rename svelte directory

--- src/test/typescript/util/string-utils.test.ts
+++ src/test/typescript/util/string-utils.test.ts
@@ -1,4 +1,4 @@
-import { replaceAfterColon } from '@/lib/util/string-utils';
+import { replaceAfterColon } from '@/svelte/util/string-utils';
 import { expect, test, describe } from 'vitest'
 
 describe('string-utils', () => {

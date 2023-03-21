566353a8114e420d1a0ca1c730e1f64ee4ba2051
Chris Dickinson <chris@dickinson.ch>
2023-03-21 22:05:32 +0100

    WIP: moving svelte into its own package

--- src/main/svelte/svelte/App.svelte
+++ src/main/svelte/svelte/App.svelte
@@ -2,7 +2,7 @@
   import Form from "./form/Form.svelte";
   import Rows from "./rows/Rows.svelte";
   import Title from "./Title.svelte";
-  import { DarkModeSwitcher } from "spring-integration-progress-bar-main-typescript/util/dark-mode";
+  import { DarkModeSwitcher } from "main-typescript/util/dark-mode";
 
   DarkModeSwitcher.initialize();
 </script>

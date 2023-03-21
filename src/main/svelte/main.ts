6caa0b4dbd98ed5239d747f85bf13e69823dfd79
Chris Dickinson <chris@dickinson.ch>
2023-03-21 22:18:24 +0100

    WIP: moving svelte into its own package

--- src/main/svelte/main.ts
+++ src/main/svelte/main.ts
@@ -1,5 +1,5 @@
 import 'main-typescript/presentation/app.css'
-import App from './svelte/App.svelte'
+import App from './lib/App.svelte'
 
 const app = new App({
   target: document.getElementById('app'),

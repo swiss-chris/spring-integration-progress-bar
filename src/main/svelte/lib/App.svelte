6ea82364cd093586e45b3f2d3ff3ffc290efbc68
Chris Dickinson <chris@dickinson.ch>
2023-03-25 17:18:51 +0100

    cleanup

--- src/main/svelte/lib/App.svelte
+++ src/main/svelte/lib/App.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
-  import Form from "./form/Form.svelte";
-  import Rows from "./rows/Rows.svelte";
-  import Title from "./Title.svelte";
-  import { DarkModeSwitcher } from "main-typescript/presentation";
+    import Form from "./form/Form.svelte";
+    import Rows from "./rows/Rows.svelte";
+    import Title from "./Title.svelte";
+    import { DarkModeSwitcher } from "main-typescript/presentation";
 
-  DarkModeSwitcher.initialize();
+    DarkModeSwitcher.initialize();
 </script>
 
 <main>
-  <div class="container">
-    <Title />
-    <Form />
-    <Rows />
-  </div>
+    <Title/>
+    <Form/>
+    <Rows/>
 </main>

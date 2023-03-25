56832be80f4cc8dae2a952ad60c579e519f52a22
Chris Dickinson <chris@dickinson.ch>
2023-03-25 20:19:11 +0100

    cleanup

--- src/test/typescript/vitest.config.ts
+++ src/test/typescript/vitest.config.ts
@@ -1,13 +1,7 @@
 import { configDefaults, defineConfig } from 'vitest/config'
-import * as path from 'path';
 
 export default defineConfig({
     test: {
         exclude: [...configDefaults.exclude, 'packages/template/*']
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, '../../main/typescript')
-        }
-    }
 })

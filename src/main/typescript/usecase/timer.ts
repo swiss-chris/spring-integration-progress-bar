1420257b0e5864108ebafa5670fec92697086ab2
Chris Dickinson <chris@dickinson.ch>
2023-04-03 21:58:07 +0200

    some basic CSS and fixing console errors

--- src/main/typescript/usecase/timer.ts
+++ src/main/typescript/usecase/timer.ts
@@ -3,10 +3,10 @@ import { Subject, Subscription } from "rxjs";
 export class OnOffTimer {
     private static readonly ONE_SECOND = 1000;
 
-    private intervalId?: NodeJS.Timer;
+    private intervalId?: number;
     private isActive = false;
     private timerTick = new Subject<void>()
-    private interval: number;
+    private readonly interval: number;
     
     constructor(interval = OnOffTimer.ONE_SECOND) {
         this.interval = interval;
@@ -29,7 +29,7 @@ export class OnOffTimer {
 
     private activate() {
         this.timerTick.next(); // the first time, execute immediately without waiting for timeout/interval.
-        this.intervalId = setInterval(() => this.timerTick.next(), this.interval);
+        this.intervalId = setInterval(() => this.timerTick.next(), this.interval) as unknown as number;
         this.isActive = true;
     }
 }
--- src/main/typescript/util/host.ts
+++ src/main/typescript/util/host.ts
@@ -2,5 +2,5 @@ import { replaceAfterColon } from './string-utils';
 
 export const getBackendUrl = (): string => {
     const url = new URL(window.location.href);
-    return `${(url.protocol)}//${(replaceAfterColon(url.host, process.env.BACKEND_PORT!))}`;
+    return `${(url.protocol)}//${(replaceAfterColon(url.host, '8080'))}`; // TODO make 8080 configurable as BACKEND_PORT
 };
